7ba861437b8b95d280dbbc097d1e7ff63c0f8cb9
Dawood AlOthman <Dawood@dawoods-macbook.lan>
2019-08-05 14:03:02 +0300

    created string task

---
+++ string_task.py
@@ -0,0 +1,10 @@
+time = input("please enter a time ")
+items = input("please enter items ")
+name = input("please enter a name ")
+scream = input("please enter a scream ")
+action = input("please enter an action ")
+
+print ("It was " + str(time) + " o'clock when I heard a knock at the door.")
+print ("I opened the door and there was a box full of " + items + " with a note saying From Mr " + name.title())
+print ("Just as I closed the door I heard a scream " + scream.upper())
+print ("I froze in place and all I could do was " + action)
